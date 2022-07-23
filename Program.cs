e685aad443c30a225be99c32cd8db1d84ababaf7
Joel Matthias <joel.matthias@envisionhealth.com>
2022-07-23 14:15:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using CliWrap.Exceptions;
+
+if (args.Length == 0)
+{
+    // Nothing to do
+    Console.WriteLine("kns: Nothing to do!");
+    return;
+}
+
+var path = args[0].Split('/');
+
+var context = path.Length > 1 ? path[0] : "";
+var ns = path.Length > 1 ? path[1] : path[0];
+
+if (!string.IsNullOrEmpty(context))
+{
+    // Set context
+    try
+    {
+        var _ = await Cli.Wrap("kubectx")
+                .WithArguments(context)
+                .ExecuteBufferedAsync();
+        Console.WriteLine($"Switched to context \"{context}\"");
+    }
+    catch (CommandExecutionException e)
+    {
+        Console.WriteLine(e.Message);
+        return;
+    }
+}
+
+if (!string.IsNullOrEmpty(ns))
+{
+    // Set namespace
+    try
+    {
+        var _ = await Cli.Wrap("kubens")
+                    .WithArguments(ns)
+                    .ExecuteBufferedAsync();
+        Console.WriteLine($"Active namespace is \"{ns}\"");
+    }
+    catch (CommandExecutionException e)
+    {
+        Console.WriteLine(e.Message);
+        return;
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+﻿## Build Package
+```
+dotnet pack
+```
+## Install (first time)
+```
+dotnet tool install --global --add-source ./nupkg Kns
+```
+## List Currently Installed Glbal Tools
+```
+dotnet tool list --global
+```
+## Install (subsequent times)
+```
+dotnet tool update --global --add-source ./nupkg Kns
+```
+## Uninstall
+```
+dotnet tool uninstall --global kns
+```
